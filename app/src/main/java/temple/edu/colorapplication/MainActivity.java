1b91d981a68086adff0ff25b12383b972785cf3b
KnoxyFresh <46945944+knoxyfresh@users.noreply.github.com>
2019-09-25 13:08:00 -0400

    working on bg color changes

--- app/src/main/java/temple/edu/colorapplication/MainActivity.java
+++ app/src/main/java/temple/edu/colorapplication/MainActivity.java
@@ -3,9 +3,11 @@ package temple.edu.colorapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.DataSetObserver;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -20,10 +22,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner = findViewById(R.id.spinner);
-        String colors[] = {"Blue","Red","Purple"};
+        Spinner myspinner = findViewById(R.id.spinner);
+        String colors[] = {"Blue","Red","Purple","green","black","white","gray"};
         ColorAdapter ca = new ColorAdapter(MainActivity.this,colors);
 
-        spinner.setAdapter(ca);
+        myspinner.setAdapter(ca);
+        myspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            findViewById(R.id.mainlayout).setBackgroundColor(Color.parseColor(adapterView.getItemAtPosition(i).toString()));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
