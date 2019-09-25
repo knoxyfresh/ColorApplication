8b55fbd700d596ebdb53b0a3bc0264d5ea7d7bc3
KnoxyFresh <46945944+knoxyfresh@users.noreply.github.com>
2019-09-25 12:25:50 -0400

    Initial commit

---
+++ app/src/main/java/temple/edu/colorapplication/ColorAdapter.java
@@ -0,0 +1,40 @@
+package temple.edu.colorapplication;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ColorAdapter extends BaseAdapter {
+
+    Context context;
+    String colors[];
+
+    public ColorAdapter(Context context, String[] colors){
+        this.context=context;
+        this.colors=colors;
+    }
+
+    @Override
+    public int getCount() {
+        return colors.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return colors[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        TextView textView
+        return null;
+    }
+}
---
+++ app/src/main/java/temple/edu/colorapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package temple.edu.colorapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SpinnerAdapter;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
